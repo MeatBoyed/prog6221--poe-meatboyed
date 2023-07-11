3ed6466774b9e310dd173ecb711083d35e02fe1f
MeatBoyed <charlierossouw@outlook.com>
2023-07-11 10:48:42 +0200

    Main window created with styles added

---
+++ ST10041239.PROG6221.POE/RideYouRent/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RideYouRent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRecipe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DisplayRecipe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ScaleRecipe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PieChart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteRecipe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
